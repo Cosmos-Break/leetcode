eb5c28e91f2f0bdc34282692137271004c835d2a
Cosmos-Break <438854233@qq.com>
2021-01-09 16:48:50 +0800

    push

--- 27.移除元素.py
+++ 27.移除元素.py
@@ -9,11 +9,13 @@ from typing import List
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
         i = 0
-        while i < len(nums):
-            if nums[i] == val:
-                nums.pop(i)
-            else:
+        for j in range(0, len(nums)):
+            if nums[j] != val:
+                nums[i] = nums[j]
                 i += 1
+        nums = nums[:i]
         return len(nums)
 # @lc code=end
 
+# 双指针, 快慢指针, 注意 这个快指针j要从0开始 
+# 因为0位置可能等于val
--- 28.实现-str-str.py
+++ 28.实现-str-str.py
@@ -7,6 +7,34 @@
 # @lc code=start
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        return haystack.find(needle)
+        n = len(needle)
+        l = len(haystack)
+        for i in range(l - n + 1):
+            if haystack[i:i + n] == needle:
+                return i
+        return -1
+
+# class Solution:
+#     def strStr(self, haystack: str, needle: str) -> int:
+#         n = len(needle)
+#         l = len(haystack)
+#         if n == 0:
+#             return 0
+#         for i in range(l - n + 1):
+#             if haystack[i] == needle[0]:
+#                 pl = i
+#                 pn = 0
+#                 while pn < n and haystack[pl] == needle[pn]:
+#                     pn += 1
+#                     pl += 1
+#                 if pn == n:
+#                     return pl - pn
+#                 else:
+#                     continue
+#         return -1
 # @lc code=end
 
+# 子串逐一比较 - 线性时间复杂度
+# 双指针 - 线性时间复杂度
+s = Solution()
+s.strStr("hello", "ll")
