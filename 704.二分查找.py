79dc2c60e2bd95479061f9713887c5d6c88b76e5
Cosmos-Break <438854233@qq.com>
2021-01-23 14:37:25 +0800

    push

---
+++ 704.二分查找.py
@@ -0,0 +1,26 @@
+#
+# @lc app=leetcode.cn id=704 lang=python3
+#
+# [704] 二分查找
+#
+
+# @lc code=start
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            mid = (left + right)//2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return -1
+
+
+# @lc code=end
+
+# right = len(nums) - 1  这里要-1
+# while left <= right:  这里要加等号
