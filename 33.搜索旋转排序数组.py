ceead2b6323d5a5b0439daaec60df3a7124c87f7
Cosmos-Break <438854233@qq.com>
2021-01-23 15:55:46 +0800

    push

--- 33.搜索旋转排序数组.py
+++ 33.搜索旋转排序数组.py
@@ -5,12 +5,34 @@
 #
 
 # @lc code=start
+# class Solution:
+#     def search(self, nums: List[int], target: int) -> int:
+#         try:
+#             index = nums.index(target)
+#         except:
+#             index = -1
+#         return index
+
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        try:
-            index = nums.index(target)
-        except:
-            index = -1
-        return index
+        if not nums:
+            return -1
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            mid = (l + r) // 2
+            if nums[mid] == target:
+                return mid
+            if nums[0] <= nums[mid]:
+                if nums[0] <= target < nums[mid]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+            else:
+                if nums[mid] < target <= nums[len(nums) - 1]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+        return -1
+
 # @lc code=end
 
