b82d99ace6b74fa9877594e39cf15bd664815788
Cosmos-Break <438854233@qq.com>
2021-03-31 21:40:36 +0800

    push

---
+++ 409.最长回文串.py
@@ -0,0 +1,19 @@
+#
+# @lc app=leetcode.cn id=409 lang=python3
+#
+# [409] 最长回文串
+#
+
+# @lc code=start
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        ans = 0
+        count = collections.Counter(s)
+        for v in count.values():
+            ans += v // 2 * 2
+            if ans % 2 == 0 and v % 2 == 1:
+                ans += 1
+        return ans
+
+# @lc code=end
+
---
+++ 414.第三大的数.py
@@ -0,0 +1,11 @@
+#
+# @lc app=leetcode.cn id=414 lang=python3
+#
+# [414] 第三大的数
+#
+
+# @lc code=start
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+# @lc code=end
+
