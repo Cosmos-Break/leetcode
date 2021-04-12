40643a20d35d3ea5952c8592fa18bf998eed1ce2
Cosmos-Break <438854233@qq.com>
2021-04-12 17:15:56 +0800

    push

---
+++ 114.二叉树展开为链表.py
@@ -0,0 +1,37 @@
+# @before-stub-for-debug-begin
+from python3problem114 import *
+from typing import *
+# @before-stub-for-debug-end
+
+#
+# @lc app=leetcode.cn id=114 lang=python3
+#
+# [114] 二叉树展开为链表
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def flatten(self, root: TreeNode) -> None:
+        preorderList = list()
+
+        def preorderTraversal(root: TreeNode):
+            if root:
+                preorderList.append(root)
+                preorderTraversal(root.left)
+                preorderTraversal(root.right)
+        
+        preorderTraversal(root)
+        size = len(preorderList)
+        for i in range(1, size):
+            prev, curr = preorderList[i - 1], preorderList[i]
+            prev.left = None
+            prev.right = curr
+
+# @lc code=end
+
