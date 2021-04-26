7d2d2a0591e57670dab0482dfb0f3b84815105ee
Cosmos-Break <438854233@qq.com>
2021-04-26 13:35:26 +0800

    push

--- 2.两数相加.py
+++ 2.两数相加.py
@@ -13,5 +13,18 @@
 
 class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        head = ListNode(l1.val + l2.val)
+        cur = head
+        while l1.next or l2.next:
+            l1 = l1.next if l1.next else ListNode()
+            l2 = l2.next if l2.next else ListNode()
+            cur.next = ListNode(l1.val + l2.val + cur.val // 10)
+            cur.val = cur.val % 10
+            cur = cur.next
+        if cur.val >= 10:
+            cur.next = ListNode(cur.val // 10)
+            cur.val = cur.val % 10
+        return head
+            
 # @lc code=end
 
--- 5.最长回文子串.py
+++ 5.最长回文子串.py
@@ -6,34 +6,28 @@
 
 # @lc code=start
 
-
-
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         n = len(s)
         ans = ""
         dp = [[False] * n for _ in range(n)]
-        for l in range(n):
+        for l in range(1, n + 1):
             for i in range(n):
-                j = i + l
+                j = i + l - 1
                 if j >= n:
                     break
-                if l == 0:
+                if l == 1:
                     dp[i][j] = True
-                elif l == 1:
-                    dp[i][j] = (s[i] == s[j])
+                elif l == 2:
+                    dp[i][j] = s[i] == s[j]
                 else:
-                    dp[i][j] = (s[i] == s[j] and dp[i + 1][j - 1])
-                if len(ans) < l + 1 and dp[i][j]:
-                    ans = s[i: j + 1]
+                    dp[i][j] = (s[i] == s[j] and dp[i+1][j-1])
+                if l > len(ans) and dp[i][j]:
+                    ans = s[i:j+1]
         return ans
 
 
 # @lc code=end
-if __name__ == "__main__":
-    s = Solution()
-    s.longestPalindrome("bbdad")
-
 # DP, 如果只有一个字符, 为回文
 # 如果有两个字符, 判断两个字符是否相同, 相同则为回文
 # 大于两个字符, 比较首尾是否相同 和 去掉首尾之后是否是回文.
